e0ed72fdacdbb5279b3240c1837a92f5e5c21c77
Jonathan Damico <jj_damico@yahoo.com>
2015-05-02 16:25:50 -0700

    countdown text

--- iHW-iOS/iHW/iHW WatchKit Extension/InterfaceController.m
+++ iHW-iOS/iHW/iHW WatchKit Extension/InterfaceController.m
@@ -16,7 +16,6 @@
 @interface InterfaceController()
 
 
-@property (nonatomic, weak) IBOutlet WKInterfaceTimer *nextPeriodStart;
 @property (weak, nonatomic) IBOutlet WKInterfaceGroup *holidayGroup;
 @property (weak, nonatomic) IBOutlet WKInterfaceGroup *dayGroup;
 @property (weak, nonatomic) IBOutlet WKInterfaceGroup *currentPeriodGroup;
@@ -24,7 +23,7 @@
 @property (weak, nonatomic) IBOutlet WKInterfaceLabel *currentPeriodName;
 @property (weak, nonatomic) IBOutlet WKInterfaceLabel *currentDay;
 @property (weak, nonatomic) IBOutlet WKInterfaceLabel *nextPeriodName;
-@property (weak, nonatomic) IBOutlet WKInterfaceTimer *nextPeriodCountdown;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *nextPeriodCountdown;
 
 
 @end
@@ -46,30 +45,48 @@
 - (void)willActivate {
     // This method is called when watch view controller is about to be visible to user
     [super willActivate];
+    
+    //If the day is a holiday...
     if([self.day isKindOfClass:[IHWHoliday class]] && ![((IHWHoliday*)self.day).name isEqualToString:@""]) {
+        
+        //Only show holiday group
         [self.holidayGroup setHidden:NO];
         [self.dayGroup setHidden:YES];
+        
     } else {
+        
+        //Don't show the holiday group, instead, display what day of the cycle it is.
         [self.holidayGroup setHidden:YES];
         [self.dayGroup setHidden:NO];
-    bool good = false;
-    int index = 1;
-    IHWPeriodCellView *view;
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:[NSString stringWithFormat:@"period%@.%d", self.date.description, index]];
-    cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    while(!good){
-        view = [[IHWPeriodCellView alloc] initWithPeriod:[self.day.periods objectAtIndex:index] atIndex:index forTableViewCell:cell];
-        if([view createCountdownViewIfNeeded]) good=true;
-    }
-    int currentPeriod = index;
+        
+        //Start at index 1 and loop until we find what period it is.
+        bool good = false;
+        int index = 1;
+        IHWPeriodCellView *view;
+        UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:[NSString stringWithFormat:@"period%@.%d", self.date.description, index]];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        while(!good){
+            view = [[IHWPeriodCellView alloc] initWithPeriod:[self.day.periods objectAtIndex:index] atIndex:index forTableViewCell:cell];
+            if([view createCountdownViewIfNeeded]) good=true;
+        }
+        
+        //
+        int currentPeriod = index;
         [self.currentPeriodName setText:getOrdinal(currentPeriod)];
         [self.currentPeriodName setHidden:NO];
-    
-    int nextPeriod = currentPeriod+1;
+        
+        int nextPeriod = currentPeriod+1;
         [self.nextPeriodName setText:getOrdinal(nextPeriod)];
         [self.nextPeriodName setHidden:NO];
         
         [self.currentDay setText:self.day.title];
+        
+        int secondsUntil = [[IHWTime now] secondsUntilTime:view.period.startTime];
+        NSString *countdownText = [NSString stringWithFormat:@"Starts in %d:%02d",secondsUntil/60, secondsUntil%60];
+        
+        [self.nextPeriodCountdown setText:countdownText];
+        
+        
     }
 }
 
