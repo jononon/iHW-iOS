31e23d71d48707e0f03594aa9c73b07b613e11da
Jonathan Damico <jj_damico@yahoo.com>
2015-05-02 13:07:25 -0700

    Create Watch Target

---
+++ iHW-iOS/iHW/iHW WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  iHW WatchKit Extension
+//
+//  Created by Jonathan Damico on 5/2/15.
+//  Copyright (c) 2015 Jonathan Burns. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
